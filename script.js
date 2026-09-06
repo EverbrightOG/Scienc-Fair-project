dd73463ba0402e4e2669cbdb6fed70ec9e3dec17
EverbrightOG <gerson.mourinho14@outlook.com>
2026-09-06 18:09:34 +0000

    Version0.2(alpha)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const modelURL = "./model/model.json";
+const metadataURL = "./model/metadata.json";
+
+let model;
+
+async function loadModel() {
+    model = await tmImage.load(modelURL, metadataURL);
+    console.log("AI model loaded!");
+}
+
 navigator.mediaDevices.getUserMedia({ video: true });
+
+loadModel();
